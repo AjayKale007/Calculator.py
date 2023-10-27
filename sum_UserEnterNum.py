f4f0ca19a090cc0f39c3e1f25bc4b5b88bb57dd9
Ajay kale <148001775+AjayKale007@users.noreply.github.com>
2023-10-27 08:17:21 +0530

    Update sum_UserEnterNum.py

--- sum_UserEnterNum.py
+++ sum_UserEnterNum.py
@@ -1,34 +1,34 @@
 # For Addition
 print("Addition")
 a=input("enter a First number: ")
-b=input("enter a First number: ")
+b=input("enter a second number: ")
 sum = int(a) + int(b)
 print(sum)
 
 #for substraction
 print("substraction")
 a=input("enter a First number: ")
-b=input("enter a First number: ")
+b=input("enter a second number: ")
 sub = int(a) - int(b)
-print(int(sub))
+print(sub)
 
 #for multiplicatin
 print("multiplication")
 a=input("enter a First number: ")
-b=input("enter a First number: ")
+b=input("enter a second number: ")
 mul = int(a) * int(b)
-print(int(mul))
+print(mul)
 
 #for Devision
 print("Devision")
 p=input("enter a First number: ")
-q=input("enter a First number: ")
+q=input("enter a Second number: ")
 Div = int(p) / int(q)
-print(int(Div))
+printt(Div)
 
 #for modulation
 print("modulation")
 a=input("enter a First number: ")
-b=input("enter a First number: ")
+b=input("enter a second number: ")
 mod = int(a) % int(b)
-print(int(mod))
+print(mod)
